a2b541f789f7565776bfa08ff6cd848c386a3d4d
Christopher James Leo <111208339+ChristopherJamesLeo@users.noreply.github.com>
2022-10-28 10:11:16 +0630

    Insert forEach loop and self avoid functin to run
    Insert forEach loop at link to preventDefault( ) and insert Self Avoid Function at the mainF function and comment on at the window.addEventListener("click",mainF)

--- Login Form/app.js
+++ Login Form/app.js
@@ -8,15 +8,18 @@ let aContainer = document.querySelector("#a-container");
 let bContainer = document.querySelector("#b-container");
 let allButtons = document.querySelectorAll(".submit");
 
-let getButtons = (e)=> e.preventDefault()
+// let getButtons = (e)=> e.preventDefault()
 let changeForm = (e)=>{
     switchCtn.classList.add("is-gx");
     setTimeout(()=>{
         switchCtn.classList.remove("is-gx");
     },1500);
     switchCtn.classList.toggle("is-txr");
-    switchCircle[0].classList.toggle("is-txr");
-    switchCircle[1].classList.toggle("is-txr");
+    switchCircle.forEach((items)=>{ // insert foreac loop
+        items.classList.toggle("is-txr")
+    })
+//     switchCircle[0].classList.toggle("is-txr");
+//     switchCircle[1].classList.toggle("is-txr");
 
     switchC1.classList.toggle("is-hidden");
     switchC2.classList.toggle("is-hidden");
@@ -24,13 +27,29 @@ let changeForm = (e)=>{
     bContainer.classList.toggle("is-txl");
     bContainer.classList.toggle("is-z200");
 }
-let mainF = (e) =>{
-    for(var i = 0 ; i < allButtons.length ; i++){
-        allButtons[i].addEventListener("click",getButtons)
-    };
+// let mainF = (e) =>{
+//     for(var i = 0 ; i < allButtons.length ; i++){
+//         allButtons[i].addEventListener("click",getButtons)
+//     };
+//     allButtons.forEach((items)=>{ // use forEach loop 
+//         items.addEventListener("click",function(){
+//             this.preventDefault();
+//         })
+//     })
+//     for(var i = 0 ; i < switchBtn.length ; i++){
+//         switchBtn[i].addEventListener("click",changeForm)
+//     }
+// }
+
+// window.addEventListener("load",mainF)
+
+(function(e){
+    allButtons.forEach((items)=>{
+        items.addEventListener("click",function(){
+            this.preventDefault();
+        })
+    })
     for(var i = 0 ; i < switchBtn.length ; i++){
         switchBtn[i].addEventListener("click",changeForm)
     }
-}
-
-window.addEventListener("load",mainF)
+})(); // self avoid function
